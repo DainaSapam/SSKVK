e6a6301c673c22925b11af478f54b0033ee17b6c
Daina_Sapam <dainasapam8@gmail.com>
2018-10-20 20:29:18 +0530

    Addition of database connection code for connecting in Miles Web Server

--- SSKVK/src/com/sskvk/jdbc/DbConnection.java
+++ SSKVK/src/com/sskvk/jdbc/DbConnection.java
@@ -1,28 +1,50 @@
 package com.sskvk.jdbc;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DbConnection {
 	public static Connection getConnection() {
 		System.out.println("Creating postgres DataBase Connection");
 		Connection con = null;
 		try {
+			//Connection from local database
 			// Provide database Driver according to your database
-			Class.forName("org.postgresql.Driver");
+			/*Class.forName("org.postgresql.Driver");
 			System.out.println("PostgreSQL driver loaded");
-			
 			// Provide URL, database and credentials according to your database 
 			con = DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/SSKVK", "postgres", "postgres");
+			"jdbc:postgresql://localhost:5432/SSKVK", "postgres", "postgres");*/
+			
+			//Connection from miles web DB
+			Properties prop = new Properties();
+			System.out.println("test");
+			prop.load(new FileInputStream(System.getProperty("user.home") + "/mydb.cfg"));
+			System.out.println("user.home: "+System.getProperty("user.home"));
+			String host = prop.getProperty("host").toString().trim();
+			String username = prop.getProperty("username").toString().trim();
+			String password = prop.getProperty("password").toString().trim();
+			String driver = prop.getProperty("driver").toString().trim();
+			
+			System.out.println("host: " + host + "\nusername: " + username 
+					+ "\npassword: " + password + "\ndriver: " + driver);
+			
+			Class.forName(driver);
+	        System.out.println("--------------------------");
+	        System.out.println("DRIVER: " + driver);
+	        con = DriverManager.getConnection(host, username, password);
+	        System.out.println("CONNECTION: " + con);
+			
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 
 		}
 		if (con != null) {
-			//System.out.println("Connection created successfully....");
+			System.out.println("Connection created successfully....");
 		}
 		return con;
 	}
-
 }
